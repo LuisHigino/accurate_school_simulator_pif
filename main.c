2693cf1401f0c3929850d9b8de2444b20564b4c6
LuisHigino <lfhm@cesar.school>
2026-04-30 11:32:19 -0300

    Código Genérico em main.c para testar raylib

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main(){
-    printf("Opa");
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Teste Raylib - Janela funcionando!");
+
+    SetTargetFPS(60);
+
+    int posX = screenWidth / 2;
+    int posY = screenHeight / 2;
+
+    while (!WindowShouldClose()) // Fecha com ESC ou botão X
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Se você está vendo isso, funcionou!", 180, 200, 20, DARKGRAY);
+        DrawCircle(posX, posY, 40, BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
