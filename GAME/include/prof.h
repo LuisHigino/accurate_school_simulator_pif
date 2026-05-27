e2920c86ff5a88b4871941f2122b71fa75f9f9e8
Dubox5 <boxwelleduardo@gmail.com>
2026-05-27 13:52:37 -0300

    Professora funcionando

--- GAME/include/prof.h
+++ GAME/include/prof.h
@@ -15,12 +15,19 @@ typedef struct {
     EstadoProfessora estadoAtual;
     int dificuldade;
     float timerAcao;
+    float timerAnimacao;
+    int idleFrame;
     Vector2 posicao;
+    Texture2D idleTexture1;
+    Texture2D idleTexture2;
+    Texture2D alertaTexture;
+    Texture2D olhandoTexture;
 } Professora;
 
 
 void InitProfessora(Professora *prof, int dificuldadeEscolhida, Vector2 posInicial);
 void UpdateProfessora(Professora *prof, float deltaTime);
 void DrawProfessora(Professora *prof);
+void UnloadProfessora(Professora *prof);
 
 #endif
--- GAME/src/prof.c
+++ GAME/src/prof.c
@@ -4,12 +4,24 @@ void InitProfessora(Professora *prof, int dificuldadeEscolhida, Vector2 posInici
     prof->estadoAtual = PROFE_ESCREVENDO;
     prof->dificuldade = dificuldadeEscolhida;
     prof->timerAcao = 0.0f;
+    prof->timerAnimacao = 0.0f;
+    prof->idleFrame = 0;
     prof->posicao = posInicial;
+    prof->idleTexture1 = LoadTexture("GAME/assets/images/professora_idle1.png");
+    prof->idleTexture2 = LoadTexture("GAME/assets/images/professora_idle2.png");
+    prof->alertaTexture = LoadTexture("GAME/assets/images/professora_olhando1.png");
+    prof->olhandoTexture = LoadTexture("GAME/assets/images/professora_olhando2.png");
 }
 
 void UpdateProfessora(Professora *prof, float deltaTime) {
 
     prof->timerAcao += deltaTime;
+    prof->timerAnimacao += deltaTime;
+
+    if (prof->timerAnimacao >= 0.4f) {
+        prof->timerAnimacao = 0.0f;
+        prof->idleFrame = 1 - prof->idleFrame;
+    }
 
     switch (prof->estadoAtual) {
         
@@ -50,33 +62,57 @@ void UpdateProfessora(Professora *prof, float deltaTime) {
 }
 
 void DrawProfessora(Professora *prof) {
-
-    Rectangle corpo = { prof->posicao.x, prof->posicao.y, 100, 250 };
+    Texture2D textureToDraw = prof->alertaTexture;
 
     switch (prof->estadoAtual) {
-        
         case PROFE_ESCREVENDO:
-            // Azul Escuro = De costas, segura
-            DrawRectangleRec(corpo, DARKBLUE);
-            DrawText("ESCREVENDO...", prof->posicao.x - 20, prof->posicao.y - 30, 20, BLUE);
+            textureToDraw = (prof->idleFrame == 0) ? prof->idleTexture1 : prof->idleTexture2;
             break;
-
         case PROFE_ALERTA:
-            // Amarelo = O "Tell" (Aviso), perigo iminente!
-            DrawRectangleRec(corpo, YELLOW);
-            DrawText("!?!?", prof->posicao.x + 25, prof->posicao.y - 50, 40, RED);
+            textureToDraw = prof->alertaTexture;
             break;
-
         case PROFE_OLHANDO:
-            // Vermelho = Olhando para a turma, abaixe o tablet!
-            DrawRectangleRec(corpo, RED);
-            DrawText("VIGIANDO!", prof->posicao.x - 10, prof->posicao.y - 30, 20, MAROON);
-            
-            // Desenha dois olhinhos brancos só pra dar um ar de que ela está virada pra frente
-            DrawCircle(prof->posicao.x + 30, prof->posicao.y + 40, 15, WHITE);
-            DrawCircle(prof->posicao.x + 70, prof->posicao.y + 40, 15, WHITE);
-            DrawCircle(prof->posicao.x + 30, prof->posicao.y + 40, 5, BLACK); // Pupila
-            DrawCircle(prof->posicao.x + 70, prof->posicao.y + 40, 5, BLACK); // Pupila
+            textureToDraw = prof->olhandoTexture;
             break;
     }
+
+    if (textureToDraw.width > 0) {
+        float scale = 12.0f;
+        DrawTexturePro(
+            textureToDraw,
+            (Rectangle){ 0.0f, 0.0f, (float)textureToDraw.width, (float)textureToDraw.height },
+            (Rectangle){ prof->posicao.x, prof->posicao.y, (float)textureToDraw.width * scale, (float)textureToDraw.height * scale },
+            (Vector2){ 0.0f, 0.0f },
+            0.0f,
+            WHITE
+        );
+    } else {
+        Rectangle corpo = { prof->posicao.x, prof->posicao.y, 100, 250 };
+
+        switch (prof->estadoAtual) {
+            case PROFE_ESCREVENDO:
+                DrawRectangleRec(corpo, DARKBLUE);
+                DrawText("ESCREVENDO...", prof->posicao.x - 20, prof->posicao.y - 30, 20, BLUE);
+                break;
+            case PROFE_ALERTA:
+                DrawRectangleRec(corpo, YELLOW);
+                DrawText("!?!?", prof->posicao.x + 25, prof->posicao.y - 50, 40, RED);
+                break;
+            case PROFE_OLHANDO:
+                DrawRectangleRec(corpo, RED);
+                DrawText("VIGIANDO!", prof->posicao.x - 10, prof->posicao.y - 30, 20, MAROON);
+                DrawCircle(prof->posicao.x + 30, prof->posicao.y + 40, 15, WHITE);
+                DrawCircle(prof->posicao.x + 70, prof->posicao.y + 40, 15, WHITE);
+                DrawCircle(prof->posicao.x + 30, prof->posicao.y + 40, 5, BLACK);
+                DrawCircle(prof->posicao.x + 70, prof->posicao.y + 40, 5, BLACK);
+                break;
+        }
+    }
+}
+
+void UnloadProfessora(Professora *prof) {
+    UnloadTexture(prof->idleTexture1);
+    UnloadTexture(prof->idleTexture2);
+    UnloadTexture(prof->alertaTexture);
+    UnloadTexture(prof->olhandoTexture);
 }
